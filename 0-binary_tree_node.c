b2666744fec32ff1472d0ae73a0d5cea488062a5
naibor <liznaibor.en@gmail.com>
2023-09-12 22:00:01 +0300

    creates a binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * binary_tree_node - Function
+ *
+ * Description: A binary tree
+ *
+ * @parent: pointer parameter of type binary_tree_t
+ * points to the parent node of the node to create
+ * @value: parameter of type int what to put in the new node
+ *
+ *
+ *Return: Return a pointer to the new node or null on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+}
